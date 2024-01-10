916d640091a03acf0155cbc694b51b372b094735
ilaydaoven <148881734+ilaydaoven@users.noreply.github.com>
2024-01-10 11:39:25 +0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { StatusBar } from "expo-status-bar";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { StyleSheet, View } from "react-native";
+import WelcomeScreen from "./src/screens/WelcomeScreen";
+import HomeScreen from "./src/screens/HomeScreen";
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Welcome">
+        <Stack.Screen
+          options={{ headerShown: false }}
+          name="Home"
+          component={HomeScreen}
+        />
+        <Stack.Screen
+          options={{ headerShown: false }}
+          name="Welcome"
+          component={WelcomeScreen}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
